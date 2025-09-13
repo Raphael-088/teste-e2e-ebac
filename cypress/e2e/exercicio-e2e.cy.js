3c176bf2ace020e4fa878d0384aa5f7a72898b50
Raphael Araujo <raphael83araujo@gmail.com>
2025-09-13 18:19:39 -0300

    Primeiro commit do projeto Cypress E2E

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,22 +1,44 @@
 /// <reference types="cypress" />
 
-context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-  /*  Como cliente 
-      Quero acessar a Loja EBAC 
-      Para fazer um pedido de 4 produtos 
-      Fazendo a escolha dos produtos
-      Adicionando ao carrinho
-      Preenchendo todas opções no checkout
-      E validando minha compra ao final */
-
+context('Fluxo de pedido - E2E', () => {
   beforeEach(() => {
-      cy.visit('/')
-  });
+    cy.fixture('usuario').as('usuario')
+  })
 
-  it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
-  });
+  it('Deve realizar um pedido completo na loja EBAC', function () {
+    cy.login(this.usuario.email, this.usuario.senha)
 
+    cy.visit('/produtos')
+    cy.adicionarProduto(1, 'XS', 'Yellow')
+    cy.adicionarProduto(2, '36', 'Brown')
+    cy.adicionarProduto(3, 'XS', 'Green')
+    cy.adicionarProduto(6, 'XL', 'Gray') // Ajuste conforme a cor correta
 
+    cy.get('.dropdown-toggle > .text-skin').click()
+    cy.visit('/checkout')
+
+    cy.get('#billing_first_name').clear().type(this.usuario.nome)
+    cy.get('#billing_last_name').clear().type(this.usuario.sobrenome)
+    cy.get('#billing_address_1').clear().type(this.usuario.endereco)
+    cy.get('#billing_city').clear().type(this.usuario.cidade)
+    cy.get('#billing_postcode').clear().type(this.usuario.cep)
+
+    cy.get('#select2-billing_country-container').click()
+    cy.get('.select2-search__field').type(this.usuario.pais)
+    cy.get('.select2-results__option').contains(this.usuario.pais).click()
+
+    cy.get('#select2-billing_state-container').click()
+    cy.get('.select2-search__field').type(this.usuario.estado)
+    cy.get('.select2-results__option').contains(this.usuario.estado).click()
+
+    cy.get('#billing_phone').clear().type(this.usuario.telefone)
+    cy.get('#billing_email').clear().type(this.usuario.email)
+
+    cy.get('#payment_method_cod').click()
+    cy.get('#terms').click()
+    cy.get('#place_order').click()
+
+    cy.contains('Obrigado. Seu pedido foi recebido.', { timeout: 10000 }).should('be.visible')
+    cy.screenshot('pedido-finalizado-com-sucesso')
+  })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,15 @@
-Cypress.Commands.add('login', (usuario, senha) => {
-    cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
-    cy.get('.woocommerce-form > .button').click()
-});
+Cypress.Commands.add('login', (email, senha) => {
+  cy.visit('/minha-conta/')
+  cy.get('#username').type(email)
+  cy.get('#password').type(senha)
+  cy.get('.woocommerce-form > .button').click()
+})
 
+Cypress.Commands.add('adicionarProduto', (index, tamanho, cor) => {
+  cy.get('.products .product').eq(index).click()
+  cy.get(`.button-variable-item-${tamanho}`).click()
+  cy.get(`.button-variable-item-${cor}`).click()
+  cy.get('.single_add_to_cart_button').click()
+  cy.visit('/product-category/clothing/men/')
+  cy.get('.products', { timeout: 10000 }).should('exist')
+})
